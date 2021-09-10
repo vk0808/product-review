8861b28a3032e6282d7189035526be5a92ecdb69
vk0808 <vk4765712@gmail.com>
2021-09-10 19:03:39 +0530

    feat: initial commit

---
+++ ProductReview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review Program\n");
+        }
+    }
+}
